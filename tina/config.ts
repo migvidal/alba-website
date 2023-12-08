f80ca8e8c5219b77d991584e5b3be61bb10512b1
migvidal <mvidal@air-institute.com>
2023-12-08 23:57:46 +0100

    Adjust config. Update dependencies

--- tina/config.ts
+++ tina/config.ts
@@ -2,22 +2,17 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
+  process.env.GITHUB_BRANCH! ||
+  process.env.HEAD! ||
   "main";
 
 export default defineConfig({
-  branch,
-
-  // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
+  branch: branch,
+  clientId: process.env.TINA_CLIENT_ID!,
   token: process.env.TINA_TOKEN,
-
   build: {
     outputFolder: "admin",
-    publicFolder: "static",
+    publicFolder: "public",
   },
   media: {
     tina: {
